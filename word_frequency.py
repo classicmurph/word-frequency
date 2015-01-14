fd35000c80a951a57bd0f2a1116fe3c2fbef26b0
Dean <deanfitzgerald@HP-Photosmart.local>
2015-01-14 17:25:21 -0500

    for testing

---
+++ word_frequency.py
@@ -0,0 +1,27 @@
+file = open("sample.txt")
+import re
+import operator
+
+
+def count_words():
+    """This will count the words and print in a (word, count) format."""
+
+
+
+    words = {}
+    file = file.lower()
+    file = file.split()
+    for word in file:
+        word = re.sub(r'[^A-Za-z0-9]',"", word)
+        if word in words:
+            words[word] += 1
+        else:
+            words[word] = 1
+    for word, count in sorted(words.items(), key=operator.itemgetter(1), reverse=True):
+        top_words = sorted(words.items(), key=lambda x: (count, word))[:]
+    for word, count in top_words:
+        print(word, count)
+
+
+
+count_words()
